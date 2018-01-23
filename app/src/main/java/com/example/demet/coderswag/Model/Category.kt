168563934f0632d1262c751f8312646af2affef4
Demeter Gabor <gabor.demeter@attentioncrm.hu>
2018-01-23 18:57:49 +0100

    lesson2

--- app/src/main/java/com/example/demet/coderswag/Model/Category.kt
+++ app/src/main/java/com/example/demet/coderswag/Model/Category.kt
@@ -3,4 +3,8 @@ package com.example.demet.coderswag.Model
 /**
  * Created by demet on 2018. 01. 23..
  */
-class Category constructor(val title: String, val Image: String)
+class Category constructor(val title: String, val Image: String){
+    override fun toString(): String {
+        return title
+    }
+}
